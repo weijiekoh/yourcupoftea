5d074e80c5093fdf38ff198eb1b7e02f63c9c3fa
Your Name <you@example.com>
2016-03-11 09:47:06 +0800

    Single result url

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 # from app import babel
-from flask import render_template, request, g
+from flask import render_template, request, g, redirect, url_for
 # from config import LANGUAGES
 # from flask.ext.babel import gettext
 
@@ -36,9 +36,10 @@ def results():
     from parties import parties
 
     results = ranking.calculate(request.form.lists(), parties, questions)
-    return render_template("results.html", 
-            results=results, parties=parties,
-            smart_share_url=smart_share_url(results))
+    return redirect(url_for("results") + "/" + smart_share_code(results))
+    # return render_template("results.html", 
+            # results=results, parties=parties,
+            # smart_share_url=request.url + "/" + smart_share_code(results))
 
 
 base64_regex = "^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$"
@@ -49,42 +50,57 @@ def results_for_fb(base64_result):
         import base64
         try:
             import urlparse
+            from parties import parties
             u = urlparse.urlparse(request.url)
             domain = '{uri.scheme}://{uri.netloc}/'.format(uri=u)
             fb_share_image = domain + "static/img/parties/at.png"
-            return render_template("result_fb_share.html", 
+            raw_result_str = base64.decodestring(base64_result)
+            sorted_results = smart_share_decode_and_sort(raw_result_str)
+            print sorted_results
+
+            return render_template("results.html", 
                     fb_share_image=fb_share_image,
-                    result_fb_str=result_fb_str(base64.decodestring(base64_result)))
+                    result_fb_str=result_fb_str(sorted_results),
+                   results=sorted_results, parties=parties)
         except (UnicodeDecodeError, AssertionError) as e:
             return render_template("index.html")
     else:
         return render_template("index.html")
 
-
-class InvalidShareString: pass
-
-def result_fb_str(result_str):
-    # TODO: input verification
+def smart_share_decode_and_sort(raw_result_str):
+    """
+    Converts a result string like "p:xx.x;q:yy.y..." to a dict
+    """
     from parties import parties
     import re
     # data verification
     veri_regex = "(?:\d:[\d\.]{2,4},){" + str(len(parties)-1) + "}\d:[\d\.]{2,4}"
     # matches something like 0:74.3,1:70.0,2:88.0,3:67.7, depending on the no. of parties in parties.py
-    assert len(re.findall(veri_regex, result_str)) > 0, "Invalid sharer url"
+    assert len(re.findall(veri_regex, raw_result_str)) > 0, "Invalid sharer url"
     display = ""
-    sorted_parties = sorted([int(x.split(":")[0]) for x in sorted(result_str.split(","))])
+    sorted_parties = sorted([int(x.split(":")[0]) for x in sorted(raw_result_str.split(","))])
 
     # make sure the input includes all the parties we have and only these parties
     assert sorted_parties == sorted(parties.keys()), "Invalid sharer url"
 
     # sort by score, not party ID:
     score_sorted_results = []
-    for r in result_str.split(","):
+    for r in raw_result_str.split(","):
         s = r.split(":")
-        score_sorted_results.append([s[0], s[1]])
+        score_sorted_results.append((int(s[0]), float(s[1])))
     score_sorted_results = sorted(score_sorted_results, key=lambda x:x[1], reverse=True)
+    return score_sorted_results
 
-    for s in score_sorted_results:
+
+class InvalidShareString: pass
+
+def result_fb_str(sorted_results):
+    """
+    Formats a dict of party_id:score etc to "ABC: xx.x%, ..."
+    """
+    from parties import parties
+    display = ""
+    for s in sorted_results:
         party_id = int(s[0])
         party_score = s[1]
         display += "%s: %s%%, " % (parties[party_id]["initials"], party_score)
@@ -92,7 +108,7 @@ def result_fb_str(result_str):
     return display[:len(display)-2]
 
 
-def smart_share_url(results):
+def smart_share_code(results):
     from parties import parties
     import base64
     url = ""
